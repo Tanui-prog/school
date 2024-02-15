d69aa0391ac9a8159c6ab2d0b86518addd6a7bf4
tanui-prog <vikiikiptoo@gmail.com>
2024-02-15 19:41:41 +0300

    modified:   core/__pycache__/urls.cpython-310.pyc 	modified:   core/__pycache__/views.cpython-310.pyc 	modified:   core/urls.py 	modified:   core/views.py 	modified:   db.sqlite3 	modified:   templates/classes.html 	modified:   templates/edit-class.html

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('addsubject', views.addsubject, name='addsubject'),
     path('add_class', views.add_class, name='add_class'),
     path('classes', views.classes, name='classes'),
+    path('saveclass/<int:class_id>/', views.saveclass, name='saveclass'),
+    path('delete_class/<int:class_id>/', views.delete_class, name='delete_class'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import redirect
 from .models import Subject, Class
 from django.contrib import messages
@@ -36,11 +36,42 @@ def classes(request):
     return render(request, 'classes.html', context)
     
 
+def saveclass(request, class_id):
+    try:
+        # Retrieve the class object
+        class_obj = Class.objects.get(class_id=class_id)
+    except Class.DoesNotExist:
+        messages.error(request, "Class does not exist.")
+        return redirect('classes')  # Redirect to classes page if class does not exist
 
-def edit_class(request, class_id):
-    classes_ = Class.objects.get(class_id=class_id)
-    return render(request, 'edit-class.html', {'classes': classes_})
+    if request.method == 'POST':
+        # Get form data
+        grade = request.POST.get('grade')
+        stream = request.POST.get('stream')
 
+        # Check if the submitted class details are unique
+        if Class.objects.exclude(class_id=class_id).filter(grade=grade, stream=stream).exists():
+            messages.error(request, "Another class with the same grade and stream already exists.")
+            return render(request, 'edit-class.html', {'class': class_obj})  # Render the edit form with error message
+
+        # Update class object
+        class_obj.grade = grade
+        class_obj.stream = stream
+        class_obj.save()
+
+        
+        return redirect('classes')  # Redirect to classes page after successful update
+    else:
+        # If not POST, render edit form
+        return render(request, 'edit-class.html', {'class': class_obj})
+    
+
+
+
+def delete_class(request, class_id):
+    class_obj = get_object_or_404(Class, class_id=class_id)
+    class_obj.delete()
+    return redirect('classes')
 
 
 def addsubject(request):
